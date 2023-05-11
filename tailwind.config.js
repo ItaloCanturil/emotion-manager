a4c2702e679327785d1ca5df502abd520f65b444
Italo Canturil <italo.cantur@gmail.com>
2023-05-10 23:38:47 -0300

    feat: add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   prefix: 'tw-',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: '#4CC9F0',
+      second: '#E2E2E2'
+    },
     extend: {},
   },
   plugins: [
