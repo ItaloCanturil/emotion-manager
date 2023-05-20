9765f518673e6e560d98314df96100a572153ea9
Italo Canturil <italo.cantur@gmail.com>
2023-05-20 20:04:51 -0300

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   prefix: 'tw-',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
       primary: '#D4ADFC',
-      second: '#E2E2E2'
+      second: '#E2E2E2',
+      ...colors
     },
     extend: {},
   },
