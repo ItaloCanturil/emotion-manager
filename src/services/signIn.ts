f5caf80859ed606f7aaa37cce1976d2bc5945c70
Italo Canturil <italo.cantur@gmail.com>
2023-07-08 18:24:32 -0300

    refactor: increase options at google function

--- src/services/signIn.ts
+++ src/services/signIn.ts
@@ -1,7 +1,22 @@
 import { supabase } from 'src/boot/supabase';
 
 export async function signInWithGoogle() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
-  });
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    });
+
+    if (error) throw error;
+
+    console.log('🚀 ~ file: signIn.ts:18 ~ signInWithGoogle ~ data:', data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
