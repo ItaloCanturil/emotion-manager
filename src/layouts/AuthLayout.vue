791d4ba7cd9d08a8211f2b61737817e1a8bad030
Italo Canturil <italo.cantur@gmail.com>
2023-07-08 18:22:47 -0300

    feat: auth pages

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-layout view="hHr Lpr lFr">
+    <q-page-container class="tw-max-w-2xl tw-mr-auto tw-ml-auto">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+
+</script>
